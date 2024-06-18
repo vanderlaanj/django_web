cd13adf0f3a343b514dd976a952561dd7bfdbf6e
vanderlaanj <99821984+vanderlaanj@users.noreply.github.com>
2024-06-18 20:05:34 +0200

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class sportovec(models.Model):
     jmeno = models.CharField(max_length=80, verbose_name='Jméno sportovce', help_text='Zadejte jméno sportovce')
-    prijmeni = models.CharField(max_lenght=80, verbose_name='Příjmení sportovce', help_text='Zadejte příjmení sportovce')
+    prijmeni = models.CharField(max_length=80, verbose_name='Příjmení spoertovce', help_text="Zadejte jméno sportovce")
     narodnost = models.CharField(max_lenght=50, verbose_name='Národnost sportovce', help_text='Zadejte národnost sportovce')
     narozeni = models.DateField(blank=True, null=True, verbose_name='Datum narození sportovce')
     vaha = models.PositiveIntegerField(blank=True, null=True, validators=[MaxValueValidator(1000), MinValueValidator(35)], verbose_name='Váha sportovce v kg')
     vyska = models.PositiveIntegerField(blank=True,null=True, validators=[MaxValueValidator(250), MinValueValidator(130)], verbose_name='Výška sportovce v cm')
-    tym= models.ForeignKey('tym',verbose_name='Tým sportovce', on_delete=models.CASCADE)
+    tym = models.ForeignKey('tym',verbose_name='Tým sportovce', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['prijmeni', 'jmeno']
